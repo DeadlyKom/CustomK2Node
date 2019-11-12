3db38d8a5ed0095f2b4f0c7c1dbab12adab32909
DeadlyKom <gma.deadly.kom@mail.ru>
2019-11-12 11:20:40 +0300

    init branch

---
+++ Source/CustomK2Node/CustomK2Node.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CustomK2Node.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CustomK2Node, "CustomK2Node" );
---
+++ Source/CustomK2Node/CustomK2NodeGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CustomK2NodeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUSTOMK2NODE_API ACustomK2NodeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
